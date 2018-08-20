c442434ad9d915fc1e3683c987fa575f6e868309
Adrienne Sunga <adriennesunga@gmail.com>
2018-08-20 14:56:51 -0400

    added drumkit sounds which play when you type the corresponding letter

---
+++ index.js
@@ -0,0 +1,71 @@
+
+
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 65) {
+      document.getElementById('audio1').play();
+    }
+  });
+
+  function play(){
+    var audio = document.getElementById("audio1");
+    audio.play();
+              }
+// ---------------A----------------------------------------------------------------
+
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 83) {
+      document.getElementById('audio2').play();
+    }
+  });
+
+
+  function play2(){
+    var audio = document.getElementById("audio2");
+    audio.play();
+              }
+
+// -------------S------------------------------------------------------------------
+
+
+document.addEventListener('keydown', function(e) {
+if (e.keyCode == 68) {
+    document.getElementById('audio3').play();
+}
+});
+
+
+function play3(){
+var audio = document.getElementById("audio3");
+audio.play();
+            }
+
+            
+// -------------D------------------------------------------------------------------
+document.addEventListener('keydown', function(e) {
+if (e.keyCode == 71) {
+    document.getElementById('audio4').play();
+}
+});
+
+
+function play4(){
+var audio = document.getElementById("audio4");
+audio.play();
+            }
+// -----------G--------------------------------------------------------------------
+
+document.addEventListener('keydown', function(e) {
+if (e.keyCode == 74) {
+    document.getElementById('audio5').play();
+}
+});
+
+
+function play5(){
+var audio = document.getElementById("audio5");
+audio.play();
+            }
+
+ // ----------------J---------------------------------------------------------------           
+
+
